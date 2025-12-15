f9b75e85279baa66df45a3562b0e9be222f66c7e
J4PC <jusah24@gmail.com>
2025-12-15 19:52:22 +0100

    Fix insert Buttone bug

--- src/main/java/me/chrr/scribble/history/command/PageInsertCommand.java
+++ src/main/java/me/chrr/scribble/history/command/PageInsertCommand.java
@@ -11,13 +11,13 @@ public class PageInsertCommand implements Command {
     }
     @Override
     public void execute(HistoryListener listener) {
-        int insertAt = insertBefore ? Math.max(0, basePage) : basePage + 1; 
+        int insertAt = insertBefore ? this.basePage : this.basePage + 1;//There is probably a better way to do this because the insert page function was originally designed to just insert before. But this works the same; it's just not elegant
         listener.scribble$history$insertPage(insertAt, null);
-    }//There is probably a better way to do this because the insert page function was originally designed to just insert before. But this works the same; it's just not elegant
+    }
 
     @Override
     public void rollback(HistoryListener listener) {
-        int insertAt = insertBefore ? Math.max(0, basePage - 1) : basePage + 1;
+        int insertAt = insertBefore ? this.basePage : this.basePage + 1;
         listener.scribble$history$deletePage(insertAt);
         if (!insertBefore) { // If the insertion was performed after the base page (Enter behavior), the view needs to switch back to the left page and not the right, which is the default behavior.
             listener.scribble$history$switchPage(basePage);
